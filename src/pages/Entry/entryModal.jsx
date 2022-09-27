7f70ee33cf27d8d2c19612b8baddbb5c4a8e8b8f
steph237 <stephaniewnkwatoh@gmail.com>
2022-09-27 16:37:07 +0100

    feature:added the entry modal

---
+++ src/pages/Entry/entryModal.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function EntryModal() {
+  const [showModal, setShowModal] = React.useState(true);
+  const [title, setTitle] = useState("");
+  const [entrybody, setEntryBody] = useState("");
+  const [date, setDate] = useState("");
+  const clearInputs = () => {
+    setTitle("");
+    setEntryBody("");
+  };
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Yayyyy!!!</h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Diary entry successfully recorded
+                  </p>
+                </div>
+                {/*footer*/}
+
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <p className="text-blue-500 pr-8">View all entries</p>
+
+                  <button
+                    className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleClose}
+                  >
+                    New entry
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
